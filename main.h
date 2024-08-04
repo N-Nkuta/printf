cea3906c4092e8624540a8dfac456587a5f453af
N-Nkuta <nokulungankuta@gmail.com>
2024-08-04 12:54:13 -0700

    Updating main.h

--- main.h
+++ main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <limits.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd>
+#include <stdio.h>
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 int printf_char(va_list val);
